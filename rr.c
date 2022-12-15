14039666c8d3d07f4f234e78c7affc7570e99913
Alfiki Diastama Afan Firdaus <89908618+alfikiafan@users.noreply.github.com>
2022-12-15 19:23:37 +0700

    Add files via upload

---
+++ rr.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int main() {
+    int i, j, n, time, remain, flag = 0, tq;
+    int TotWT = 0, TotTA = 0, AT[100], b[100], rt[100];
+
+    printf("\nMasukkan jumlah proses\t\t\t: ");
+    scanf("%d", &n);
+    remain = n;
+
+    for(i = 0; i<n; i++) {
+        printf("\nMasukkan arrival time untuk proses P%d   : ", i+1);
+        scanf("%d", &AT[i]);
+        printf("Masukkan burst time untuk proses P%d\t: ", i+1);
+        scanf("%d", &b[i]);
+        rt[i]= b[i];
+    }
+    printf("\nMasukkan time quantum\t\t\t: ");
+    scanf("%d", &tq);
+    printf("\n--------------------------------------------");
+    printf("\n| Proses | Turn Around Time | Waiting Time |\n");
+    printf("--------------------------------------------\n");
+
+    for(time = 0, i = 0; remain != 0;) {
+        if(rt[i] <= tq && rt[i] > 0) {
+            time += rt[i];
+            rt[i] = 0;
+            flag = 1;
+        }
+        else if(rt[i] > 0) {
+            rt[i] -= tq;
+            time += tq;
+        }
+        if(rt[i] == 0 && flag == 1) {
+            remain--;
+            printf("|   P%d   |\t   %d\t    |\t    %d      |\n", i+1, time-AT[i], time-AT[i] - b[i]);
+            TotWT += time - AT[i] - b[i];
+            TotTA += time - AT[i];
+            flag = 0;
+        }
+        if(i == n-1)
+            i = 0;
+        else if(AT[i + 1] <= time)
+            i++;
+        else
+            i = 0;
+    }
+    printf("--------------------------------------------\n");
+
+    printf("\nAverage Waiting Time    = %f\n", TotWT * 1.0/n);
+    printf("Average Turnaround Time = %f\n\n", TotTA * 1.0/n);
+
+    return 0;
+}
